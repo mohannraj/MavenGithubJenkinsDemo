7291f7196d6c3570bd45905544efd116fe631a54
Mohan Raj Lingareddy <mohanraj.lingareddy@gmail.com>
2018-02-03 23:30:44 +0530

    my mavenGithubJenkinsDemo

---
+++ src/qsp/MavenProjectDemo1.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.testng.annotations.Test;
+
+public class MavenProjectDemo1 {
+	@Test
+	public void mavenDemo2()
+	{
+		System.out.println("welcome to maven project demo2");
+	}
+
+}
