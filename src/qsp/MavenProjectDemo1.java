0ea7909857b5052df164c4b79b161fee7986f916
Mohan Raj Lingareddy <mohanraj.lingareddy@gmail.com>
2018-02-05 16:32:18 +0530

    Updated the Reporter log.

--- src/qsp/MavenProjectDemo1.java
+++ src/qsp/MavenProjectDemo1.java
@@ -1,12 +1,13 @@
 package qsp;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class MavenProjectDemo1 {
 	@Test
 	public void mavenDemo2()
 	{
-		System.out.println("welcome to maven project demo2");
+		Reporter.log("welcome to maven project demo2");
 	}
 
 }
